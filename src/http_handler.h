57f02dd5d625407797e511e9d0ba0c169eb4e8d1
Kritika Tewari <kritikatewari123@gmail.com>
2025-10-09 07:06:44 +0530

    Update http_handler.h

--- src/http_handler.h
+++ src/http_handler.h
@@ -1,8 +1,6 @@
 #ifndef HTTP_HANDLER_H
 #define HTTP_HANDLER_H
-
 #include "proxy_parse.h"   // Ensure this defines struct ParsedRequest
-
 // HTTP request handlers
 int handle_get(int clientSocket, struct ParsedRequest* request, char* raw_request);
 int handle_post(int clientSocket, struct ParsedRequest* request, char* raw_request);
@@ -10,6 +8,4 @@ int handle_file_upload(int clientSocket, struct ParsedRequest* request, char* bo
 int handle_file_download(int clientSocket, struct ParsedRequest* request);
 int handle_find(int clientSocket, struct ParsedRequest* request, char* raw_request);
 int handle_put(int clientSocket, struct ParsedRequest* request, char* raw_request);
-
-
 #endif
